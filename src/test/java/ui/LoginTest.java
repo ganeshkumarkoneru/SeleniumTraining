88cd6c8b02301395e56dae518ebb14fd0146d2dc
ganeshkumarkoneru <100346060+ganeshkumarkoneru@users.noreply.github.com>
2025-04-04 10:55:30 +0530

    Update LoginTest.java

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -15,8 +15,8 @@ public class LoginTest {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("ganeshkumargkkoneru@gmail.com");
-		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Vinayakgk@26");
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("ganeshneru@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("45678942514");
 		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
